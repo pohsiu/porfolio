7f3010d7b48415119f5628f9c617b93b744abf17
Louis <louis.hsiao@d8ai.com>
2020-01-20 13:13:36 +0800

    test: parallex scroll effect

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -33,6 +33,69 @@ const useStyles = makeStyles(theme => ({
   content: {
     height: 1000,
   },
+  tlWrapper: {
+    width: '100%',
+    float: 'left',
+    position: 'relative',
+  },
+  heroFixed: {
+    opacity: 1,
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    height: '100vh',
+    marginLeft: '50%',
+    zIndex: 1,
+    transform: 'translateX(-50%)',
+    width: '100vw',
+  },
+  heroVisual: {
+    filter: 'blur(0)',
+    transform: 'scale(1)',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    transition: 'filter .5s cubic-bezier(1,0,0,1) .5s,transform .5s cubic-bezier(1,0,0,1) .5s,-webkit-filter .5s cubic-bezier(1,0,0,1) .5s,-webkit-transform .5s cubic-bezier(1,0,0,1) .5s',
+  },
+  heroWrap: {
+    display: 'flex',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    margin: '0 auto',
+    zIndex: 1,
+    maxWidth: 1440,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  outer: {
+    position: 'relative',
+    zIndex: 1,
+    overflowX: 'auto',
+    background: 'linear-gradient(transparent,transparent 100vh,#2a2f36 100.001vh,#2a2f36)'
+  },
+  outerContainer: {
+    width: '100%',
+    margin: '0 auto',
+    maxWidth: 1440,
+  },
+  heroFilled: {
+    position: 'relative',
+    top: '-6.5rem',
+    left: '50%',
+    width: '100vw',
+    height: '100vh',
+    transform: 'translateX(-50%)',
+  },
+  fullWidth: {
+    display: 'block',
+    width: '100%',
+    margin: '0 auto',
+    maxWidth: 1440,
+    height: 1000,
+    overflowY: 'auto',
+  },
+
 }));
 
 const imgs = [
@@ -43,15 +106,26 @@ const About = (props) => {
   const classes = useStyles();
   return (
     <div className={classes.app}>
-      <div className={classes.parallex}/>
-      <Scene 
-        image1={imgs[0]} 
-        image2={imgs[1]}
-        className={classes.background}
-      />
-      
-      <div className={classes.content}>
-        About
+      <div className={classes.tlWrapper} style={{ zIndex: 1, opacity: 1 }}>
+        <div className={classes.heroFixed} style={{ display: 'block', backgroundColor: 'rgb(255, 145, 145)'}}>
+          <div className={classes.heroVisual} />
+          <div className={classes.heroWrap}>
+            <h1>About Me</h1>
+          </div>
+        </div>
+        <div className={classes.outer}>
+          <div className={classes.outerContainer}>
+            <nav>NAV BAR</nav>
+            <div style={{ opacity: 1 }}>
+              <div className={classes.heroFilled} />
+              <div className={classes.fullWidth}>
+                <div>
+                  This is the content below the fullWidth part ...
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+// --large-viewport: 1440px;
+// --medium-viewport: 1024px;
+// --small-viewport: 900px;
+// --xsmall-viewport: 568px;
+// --max-width: 1440px;
+// --c--page-background: #2a2f36;
+// --c--white: #fff;
+// --c--gray-border: #485059;
+// --c--planetary-red: #dc4133;
+// --c--text: #fff;
+// --c--text-secondary: #9da7b3;
+// --scroll-bar: 15px;
+// blue theme
+// #034f84
+// #92a8d1
+// #d5f4e6
+// #80ced6
+
 export default createMuiTheme({
   palette: {
     primary: {
