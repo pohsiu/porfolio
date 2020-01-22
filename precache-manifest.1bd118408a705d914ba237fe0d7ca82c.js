e721d3efe646e766c6ba5c53f220a0252ad1a7bf
Louis Hsiao <louis.hsiao@d8ai.com>
2020-01-22 19:42:13 +0800

    Updates

---
+++ precache-manifest.1bd118408a705d914ba237fe0d7ca82c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60f85c8120a3f83dfce901c15467895",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "8493d83162ada55bef92",
+    "url": "/porfolio/static/js/2.3dbd8273.chunk.js"
+  },
+  {
+    "revision": "5e045e94752ceaf3b090",
+    "url": "/porfolio/static/js/main.7ee5f211.chunk.js"
+  },
+  {
+    "revision": "087a855a2c006e0368c9",
+    "url": "/porfolio/static/js/runtime~main.a24bf919.js"
+  }
+]);
