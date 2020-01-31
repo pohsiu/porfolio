f5989f8196e35268a88fde779c7b2edba887d50e
Louis Hsiao <louis.hsiao@d8ai.com>
2020-01-31 20:01:03 +0800

    Updates

---
+++ precache-manifest.a2bfafcea4543dbce602d1c7e7613be7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b31eb2f7c6ec12fb02971aad1ad123",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "f4cfde4989dd7e5dbdf3",
+    "url": "/porfolio/static/js/2.fa993044.chunk.js"
+  },
+  {
+    "revision": "e62d22928ee9eaf37dbe",
+    "url": "/porfolio/static/js/main.af7aa3e6.chunk.js"
+  },
+  {
+    "revision": "f0c16158600afe27a9d5",
+    "url": "/porfolio/static/js/runtime~main.46281451.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/porfolio/precache-manifest.8f8af8f5d54b7cdf901abe1fa6479ff5.js"
+  "/porfolio/precache-manifest.a2bfafcea4543dbce602d1c7e7613be7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/porfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
